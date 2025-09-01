ca43773102b6a1bc1ed5872e6065031b99bc6fed
𝚨𝛂ѕͱ꧊𝐢͢к <teamaashik@gmail.com>
2025-09-01 20:35:34 +0530

    Update __main__.py

--- TEAMZYRO/__main__.py
+++ TEAMZYRO/__main__.py
@@ -1,24 +1,27 @@
-import sys
 import asyncio
+import sys
 import importlib
 
 from pyrogram import idle
 from pytgcalls.exceptions import NoActiveGroupCall
 
 import config
-from TEAMZYRO import LOGGER, app, userbot
+from TEAMZYRO.core.bot import ZYRO
+from TEAMZYRO.core.userbot import Userbot
+from TEAMZYRO import LOGGER, api
 from TEAMZYRO.core.call import ZYRO as ZYROClass
 from TEAMZYRO.misc import sudo
 from TEAMZYRO.plugins import ALL_MODULES
 from TEAMZYRO.utils.database import get_banned_users, get_gbanned
 from config import BANNED_USERS
 
+# ------------------ Initialize clients here ------------------
+app = ZYRO()
+userbot = Userbot()
 
 async def init():
     if not any([config.STRING1, config.STRING2, config.STRING3, config.STRING4, config.STRING5]):
-        LOGGER(name).error(
-            "𝐒𝐭𝐫𝐢𝐧𝐠 𝐒𝐞𝐬𝐬𝐢𝐨𝐧 𝐍𝐨𝐭 𝐅𝐢𝐥𝐥𝐞𝐝, 𝐏𝐥𝐞𝐚𝐬𝐞 𝐅𝐢𝐥𝐥 𝐀 𝐏𝐲𝐫𝐨𝐠𝐫𝐚𝐦 𝐒𝐞𝐬𝐬𝐢𝐨𝐧"
-        )
+        LOGGER(__name__).error("𝐒𝐭𝐫𝐢𝐧𝐠 𝐒𝐞𝐬𝐬𝐢𝐨𝐧 𝐍𝐨𝐭 𝐅𝐢𝐥𝐥𝐞𝐝 ❌")
         sys.exit(1)
 
     await sudo()
@@ -49,32 +52,22 @@ async def init():
     bot = ZYROClass()
     await bot.start()
 
-    # Attempt to stream
     try:
         await bot.stream_call("https://te.legra.ph/file/29f784eb49d230ab62e9e.mp4")
     except NoActiveGroupCall:
-        LOGGER("TEAMZYRO").error(
-            "𝗣𝗹𝗭 𝗦𝗧𝗔𝗥𝗧 𝗬𝗢𝗨𝗥 𝗟𝗢𝗚 𝗚𝗥𝗢𝗨𝗣 𝗩𝗢𝗜𝗖𝗘𝗖𝗛𝗔𝗧\n\n𝗕𝗔𝗕𝗬 𝗟𝗢𝗕 𝗬𝗢𝗨 𝗕𝗢𝗧 𝗦𝗧𝗢𝗣........"
-        )
+        LOGGER("TEAMZYRO").error("𝗣𝗹𝗲𝗮𝘀𝗲 𝗦𝘁𝗮𝗿𝘁 𝗬𝗼𝘂𝗿 𝗟𝗼𝗴 𝗚𝗿𝗼𝘂𝗽 𝗩𝗼𝗶𝗰𝗲𝗖𝗵𝗮𝘁 ❗")
         sys.exit(1)
-    except Exception:
-        pass
 
-    # Call decorators if any
     await bot.decorators()
 
-    LOGGER("TEAMZYRO").info(
-        "╔═════ஜ۩۞۩ஜ════╗\n  ☠︎︎𝗠𝗔𝗗𝗘 𝗕𝗬 𝗠𝗥 𝗞𝗔𝗥𝗠𝗔☠︎︎\n╚═════ஜ۩۞۩ஜ════╝"
-    )
+    LOGGER("TEAMZYRO").info("✅ Bot Successfully Started!")
 
     await idle()
 
-    # Stop all clients
     await app.stop()
     await userbot.stop()
     await bot.stop()
-    LOGGER("TEAMZYRO").info("𝗦𝗧𝗢𝗣 𝗕𝗔𝗕𝗬 𝗟𝗢𝗕 𝗠𝗨𝗦𝗜𝗖 🎻 𝗕𝗢𝗧")
+    LOGGER("TEAMZYRO").info("🛑 Bot Stopped")
 
-
-if name == "__main__":
+if __name__ == "__main__":
     asyncio.run(init())
